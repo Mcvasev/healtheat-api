bdbb978adf7813962374a30954453c77fd8a216a
Islam Alaev <alaev095@bk.ru>
2021-06-07 16:26:55 +0300

    TestIvan

--- recipe.js
+++ recipe.js
@@ -5,7 +5,8 @@ const recipeSchema = new Schema({
     type: String,
     title: String,
     description: String,
-    ingredients: Array
+    ingredients: Array,
+    imgrec: String
 })
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const Recipe = require('./recipe')
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb+srv://Slam:1234@cluster.txjoy.mongodb.net/healtheat?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb+srv://Slam:1234@cluster.txjoy.mongodb.net/healtheat?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 
@@ -19,27 +21,34 @@ db.once('open', () => {
     console.log('Connection to db established');
 });
 
-app.get('/api/recipe', (req,res) =>{
-    Recipe.find().then((recipe)=>{
-        res.json(recipe);
-    })
+app.get('/api/recipe', (req, res) => {
+    if (!req.query.search) {
+        Recipe.find().then((recipe) => {
+            res.json(recipe);
+        })
+    } else {
+        Recipe.find({ title: new RegExp(req.query.search)}).then((recipe) => {
+            res.json(recipe);
+        })
+    }
 })
 
-app.get('/api/recipe/:id', (req,res) =>{
-   
-    Recipe.find({_id : req.params.id})
-    .then((item) =>{
-      if (!item) {
-          res.status(404).send('error');
-      }
-      res.status(200).json(item);
-    })
-    .catch(()=>{
-        res.status(404).send('error');
-    })
+app.get('/api/recipe/:id', (req, res) => {
+
+    Recipe.findOne({ _id: req.params.id })
+        .then((item) => {
+            if (!item) {
+                res.status(404).send('error');
+            }
+            console.log(item)
+            res.status(200).json(item);
+        })
+        .catch(() => {
+            res.status(404).send('error');
+        })
 })
 
 
-app.listen(3001,()=>{
+app.listen(3001, () => {
     console.log("server is on port 3001")
 })
